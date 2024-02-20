338e14ba3eda9c32521082f6ad6393dd1d301fcc
Nitesh Kumar <imnksid@gmail.com>
2024-02-20 14:00:09 +0530

    Created GuessInfo component with all APIs & logic

---
+++ guess-about-you/src/components/guess-info.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import React, { useState } from "react";
+import styles from "../app/page.module.css";
+import {
+  fetchAge,
+  fetchGender,
+  fetchNationality,
+} from "../api/guess-info-apis";
+import countryCodes from "../constants/country-codes";
+
+const GuessInfo = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [country, setCountry] = useState("");
+
+  const handleFormSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    if (!name) {
+      alert("Name is required.");
+      return;
+    }
+
+    try {
+      const agifyResponse = await fetchAge(name);
+      const guessedAge = agifyResponse?.age || "Unknown";
+      setAge(guessedAge);
+
+      const genderizeResponse = await fetchGender(name);
+      const guessedGender = genderizeResponse?.gender || "Unknown";
+      setGender(guessedGender);
+
+      const nationalizeResponse = await fetchNationality(name);
+
+      const countries = nationalizeResponse?.country;
+      if (countries && countries.length > 0) {
+        const guessedCountries = codeToCountry(countries);
+        setCountry(guessedCountries);
+      } else {
+        setCountry("Unknown");
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit} className={styles.form}>
+        <div className={styles.description}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <button
+            type="submit"
+            className={styles.button}
+            // style={{ borderRadius: "10px", padding: 10, fontSize: 18 }}
+          >
+            Guess Info
+          </button>
+        </div>
+      </form>
+      {age && <p>Guessed Age: {age}</p>}
+      {gender && <p>Guessed Gender: {gender}</p>}
+      {country && <p>Guessed Country: {country}</p>}
+    </>
+  );
+};
+
+export default GuessInfo;
+
+const codeToCountry = (data: any[]) => {
+  const countriesArray = data.map((country: { country_id: string }) => {
+    return countryCodes[country.country_id];
+  });
+  const countries = countriesArray.join(", ");
+  return countries;
+};
